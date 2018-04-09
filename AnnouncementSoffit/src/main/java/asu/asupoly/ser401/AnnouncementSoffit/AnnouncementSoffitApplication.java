ff11fb0eee4d158cfa9e290e95488566f42b49a5
David Worrell <Aramis14@gmail.com>
2018-04-09 00:15:38 -0700

    Added the minimal soffit architecture

--- AnnouncementSoffit/src/main/java/asu/asupoly/ser401/AnnouncementSoffit/AnnouncementSoffitApplication.java
+++ AnnouncementSoffit/src/main/java/asu/asupoly/ser401/AnnouncementSoffit/AnnouncementSoffitApplication.java
@@ -1,8 +1,10 @@
 package asu.asupoly.ser401.AnnouncementSoffit;
 
+import org.apereo.portal.soffit.renderer.SoffitApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SoffitApplication
 @SpringBootApplication
 public class AnnouncementSoffitApplication {
 
