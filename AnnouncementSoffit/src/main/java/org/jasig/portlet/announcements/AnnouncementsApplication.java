876d71ba3f5a7af2449b20842bc02893818a1e3c
David Worrell <Aramis14@gmail.com>
2018-04-27 22:06:14 -0700

    Updated to my own project files. Sorry guys

---
+++ AnnouncementSoffit/src/main/java/org/jasig/portlet/announcements/AnnouncementsApplication.java
@@ -0,0 +1,14 @@
+package org.jasig.portlet.announcements;
+
+import org.apereo.portal.soffit.renderer.SoffitApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SoffitApplication
+public class AnnouncementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnouncementsApplication.class, args);
+	}
+}
---
+++ AnnouncementSoffit/src/main/java/org/jasig/portlet/announcements/config/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package org.jasig.portlet.announcements.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SuppressWarnings("unused")
+public class DatabaseConfig {
+}
